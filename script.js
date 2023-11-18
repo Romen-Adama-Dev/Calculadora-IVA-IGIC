f73dbbb163f48d6e152e036b043a015e440f2f3e
Romen Adama Caetano Ramirez <romen-dev@MacMini.local>
2023-11-18 17:31:06 +0000

    Actualizacion Colores y se anadio el apartado del coste del IVA y del IGIC

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ document.getElementById('calculadora').addEventListener('submit', function(event
     
     const precioIVA = parseFloat(document.getElementById('precioIVA').value);
     const precioBase = precioIVA / 1.21;
+    const cantidadIVA = precioIVA - precioBase;
     const precioIGIC = precioBase * 1.07;
+    const cantidadIGIC = precioIGIC - precioBase;
     
-    document.getElementById('resultado').innerHTML = `
-        Precio sin impuestos: ${precioBase.toFixed(2)}€<br>
-        Precio con IGIC (7%): ${precioIGIC.toFixed(2)}€
-    `;
+    document.querySelector('.precioBase').textContent = `Precio sin impuestos: ${precioBase.toFixed(2)}€`;
+    document.querySelector('.cantidadIVA').textContent = `Cantidad correspondiente al IVA: ${cantidadIVA.toFixed(2)}€`;
+    document.querySelector('.precioIGIC').textContent = `Precio con IGIC (7%): ${precioIGIC.toFixed(2)}€`;
+    document.querySelector('.cantidadIGIC').textContent = `Cantidad correspondiente al IGIC: ${cantidadIGIC.toFixed(2)}€`;
 });
